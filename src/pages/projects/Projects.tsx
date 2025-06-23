51b6398e484c7bba1b8ef5e2beacbb552f5a2c70
Xfive <xfive@aizen.home>
2025-06-23 18:43:58 +0000

    Correction of mistakes in Projects file

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useTranslation } from 'react-i18next';
 import { getProjects } from "../../services/githubCMS";
-import clientProjects, { ClientProject } from '../../data/clientProjects';
+import clientProjects from '../../data/clientProjects';
 import ConfidentialBadge from "../../components/ConfidentialBadge";
 
 const Projects: React.FC = () => {
